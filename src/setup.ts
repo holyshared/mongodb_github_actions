1d8bd18ae250d81a8bf045bcbf066e72631baba2
holyshared <holy.shared.design@gmail.com>
2021-01-03 20:33:23 +0900

    fix url

--- src/setup.ts
+++ src/setup.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 import { User } from './';
 
 before(async () => {
-  const url = process.env.MONGO_URL || 'mongodb://example:example@localhost:27017/example?replicaSet=replset';
+  const url = process.env.MONGO_URL || 'mongodb://localhost:27017/test?replicaSet=replset'; // XXX need db name?
   await mongoose.connect(url, {
     useNewUrlParser: true,
     useCreateIndex: true,
